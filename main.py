f851995788426a58e12e997db19a2907a1c3fdab
coffeer <coffeer6615@gmail.com>
2020-08-07 20:58:02 +0800

    Default Changelist

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+
+import PySide2
+from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2 import QtCore
+
 from UI import MainWindowsLayout
+
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle('Fusion')
     w_Main = QMainWindow()
     ui = MainWindowsLayout.Ui_MainWindow()
     ui.setupUi(w_Main)
