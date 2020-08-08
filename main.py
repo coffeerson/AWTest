94330824ade7334fbf9ef1516d157bad83ff9d7c
coffeer <coffeer6615@gmail.com>
2020-08-09 01:20:45 +0800

    Default Changelist

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from ui_function import *
 
 class OptionSelect(QMainWindow):
     def __init__(self):
-        # QMainWindow.__init__(self)
         # 从文件中加载UI定义
         # 从 UI 定义中动态 创建一个相应的窗口对象#
         # 注意：里面的控件对象也成为窗口对象的属性了
