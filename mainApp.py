73d413532386a064a711c86b3337148eee496859
coffeer <coffeer6615@gmail.com>
2020-08-31 23:32:53 +0800

    none

--- mainApp.py
+++ mainApp.py
@@ -6,5 +6,4 @@ from PySide2.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     engine = QQmlApplicationEngine("UI\\MapEdit.qml")
-
     app.exec_()
