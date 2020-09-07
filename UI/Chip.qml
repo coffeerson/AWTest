be77e29307ec3794733273541d8b9e531b56edb9
coffeer <coffeer6615@gmail.com>
2020-09-07 23:01:38 +0800

    添加chip.qml

---
+++ UI/Chip.qml
@@ -0,0 +1,12 @@
+import QtQuick 2.15
+
+Item{
+    id: chips
+    width: 40
+    height: 40
+
+    Rectangle {
+        id: rectangle
+        border.color: "black"
+    }
+}
--- mainApp.py
+++ mainApp.py
@@ -7,13 +7,12 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     engine = QQmlApplicationEngine("UI\\MapEdit.qml")
 
-    ## -->获取根组件，如果没有这一行，会导致 “Internal C++ object (PySide2.QtCore.QObject) already deleted.“ 错误
+    # -->获取根组件，如果没有这一行，会导致 “Internal C++ object (PySide2.QtCore.QObject) already deleted.“ 错误
     rootObj = engine.rootObjects()[0]
 
-    ## -->获取objectname为test的组件
+    # -->获取objectName为test的组件
     testObject = rootObj.findChild(QObject, "test")
-    ## -->修改该组件的参数
+    # -->修改该组件的参数
     testObject.setProperty("width", 100)
 
     sys.exit(app.exec_())
-
