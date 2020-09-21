d24db2c2c231e82f30a5ba7d6d18c86690cff320
coffeer <coffeer6615@gmail.com>
2020-09-21 20:29:49 +0800

    添加chip.qml

--- mainApp.py
+++ mainApp.py
@@ -8,12 +8,11 @@ if __name__ == "__main__":
     engine = QQmlApplicationEngine("UI\\MapEdit.qml")
 
     # -->获取根组件，如果没有这一行，会导致 “Internal C++ object (PySide2.QtCore.QObject) already deleted.“ 错误
-    #$rootObj = engine.rootObjects()[0]
+    rootObj = engine.rootObjects()[0]
 
     # -->获取objectName为test的组件
-    #testObject = rootObj.findChild(QObject, "test")
+    testObject = rootObj.findChild(QObject, "test")
     # -->修改该组件的参数
-    #testObject.setProperty("width", 100)
-
+    testObject.setProperty("width", 100)
 
     sys.exit(app.exec_())
